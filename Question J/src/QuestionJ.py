473425b47b22b25119be8e12750ef098b9761f39
Tommy Lamb <james003pond@gmail.com>
2016-11-08 17:47:16 +0000

    Updated Question J

--- Question J/src/QuestionJ.py
+++ Question J/src/QuestionJ.py
@@ -1,14 +1,24 @@
 #Question 5
 def evennumbers(n):
-    return [list for list in range(2*n) if list % 2 == 0]
+    return [number for number in range(0,2*n,2)]
+
+evennumbers(10)
+
+
 
 #Question 6
 def multiples(numberlist):
     return [sublist for sublist in numberlist if sublist % 3 == 0]
 
+multiples(list(range(36)))
+
+
 #Question 7
 def selfzip(listx, listy):
     return [(listx[a],listy[a]) for a in range(max(len(listx),len(listy)))]
+
+selfzip(['a','b','c','d','e','f','g','h','i','j','k','l'],list(range(12)))
+
 
 #Question 8
 def squarepentagonals(length):
@@ -17,6 +27,18 @@ def squarepentagonals(length):
 squarepentagonals(10)
 squarepentagonals(10000)
 
+from math import floor, sqrt
+def squarepentagonalgenerator():
+    index = 0
+    while True:
+        x = index*(3*index-1)//2
+        if floor(sqrt(x))==sqrt(x):
+            yield x
+        index += 1
+
+SPNumber = squarepentagonalgenerator()
+for i in range(4):
+    next(SPNumber)
 
 #Question 9
 def nest(n):
@@ -24,7 +46,9 @@ def nest(n):
         return []
     else:
         return [nest(n-1)]
-    
+
+nest(5)
+
 
 def unnest(n):
     try:
@@ -32,13 +56,22 @@ def unnest(n):
         return 1+unnest(n[0])
     except IndexError:
         return 0
-    
+
+unnest(nest(5))
+unnest([[[]]])   
+
+
 def nestadd(x,y):
     try:
         x[0]
         return [nestadd(x[0], y)]
     except IndexError:
         return y
+
+x = nestadd(nest(5),nest(4))
+print(x)
+unnest(x)
+
 
 def nestmult(x,y):
     z = x
@@ -52,6 +85,10 @@ def nestmult(x,y):
             return accum
 
 
-x = nest(5)
-y = nest(4)
-z = nestadd(x,y)
+x = nestmult(nest(5), nest(4));
+print(x)
+unnest(x)
+nestmult(nest(0), nest(5));
+nestmult(nest(5), nest(0));
+nestmult(nest(0), nest(0));
+nestmult(nest(2), nest(1));
